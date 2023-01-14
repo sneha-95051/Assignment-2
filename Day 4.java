9be8858bb9537cf405ffba6cc6ebc560fd9480a6
sneha-95051 <122288752+sneha-95051@users.noreply.github.com>
2023-01-14 21:15:38 +0530

    Create Day 4.java

---
+++ Day 4.java
@@ -0,0 +1,12 @@
+//Write a program to find the sum of 5 integers.
+
+public class Main {
+    public static void main(String[] args) {
+        int s=0;
+        for (int i=0;i<=5;i++)
+        {
+            s +=i;
+        }
+        System.out.print(s);
+    }
+}
